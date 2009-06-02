227bb67492f623b7a68a361a216d54ed985b2126
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-06-02 19:06:18 +0000

    *** empty log message ***
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@290 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/Communication/sps/H1/test/H1Test.java
+++ at/redeye/Communication/sps/H1/test/H1Test.java
@@ -1,9 +1,7 @@
 package at.redeye.Communication.sps.H1.test;
 
 import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 
 import at.redeye.Communication.sps.H1.comm.IH1Communication;
 import at.redeye.Communication.sps.H1.comm.impl.H1ConnectionDefinition;
@@ -23,13 +21,10 @@ public class H1Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		IH1Communication h1comm = H1TCPHandling.getInstance(new H1ConnectionDefinition("127.0.0.1", 9001,
-						"LLR-PFA1", "LLR-PFA1"), logger);
-		PatternLayout layout = new PatternLayout(
-				"%d{ISO8601} %-5p (%F:%L): %m%n");
-		ConsoleAppender consoleAppender = new ConsoleAppender(layout);
-		logger.addAppender(consoleAppender);
-
+		BasicConfigurator.configure();
+		IH1Communication h1comm = H1TCPHandling.getInstance(new H1ConnectionDefinition("", 
+						"", ""), logger);
+		
 		h1comm.handleH1Request();
 
 		H1ReceiverThread poller = new H1ReceiverThread(h1comm, logger);
