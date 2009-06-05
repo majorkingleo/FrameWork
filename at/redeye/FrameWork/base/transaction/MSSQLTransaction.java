2961f0b8da50d8dbd1a3fe7c0804268654817977
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-06-05 18:04:38 +0000

    Brief filter implementation
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@291 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/base/transaction/MSSQLTransaction.java
+++ at/redeye/FrameWork/base/transaction/MSSQLTransaction.java
@@ -6,6 +6,8 @@ package at.redeye.FrameWork.base.transaction;
 import java.sql.SQLException;
 
 import at.redeye.FrameWork.base.bindtypes.DBDateTime;
+import at.redeye.FrameWork.widgets.DBFilterComboBox;
+import at.redeye.FrameWork.widgets.DBFilterEditField;
 import at.redeye.SqlDBInterface.SqlDBConnection.impl.ConnectionDefinition;
 import at.redeye.SqlDBInterface.SqlDBConnection.impl.MissingConnectionParamException;
 import at.redeye.SqlDBInterface.SqlDBConnection.impl.UnSupportedDatabaseException;
@@ -22,149 +24,208 @@ import org.joda.time.DateMidnight;
  */
 public class MSSQLTransaction extends Transaction {
 
-	/**
-	 * 
-	 */
-	protected MSSQLTransaction() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     *
+     */
+    protected MSSQLTransaction() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param def
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
-	 * @throws MissingConnectionParamException
-	 * @throws UnSupportedDatabaseException
-	 */
-	public MSSQLTransaction(ConnectionDefinition def)
-			throws ClassNotFoundException, SQLException,
-			MissingConnectionParamException, UnSupportedDatabaseException {
-		super(def);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param def
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws MissingConnectionParamException
+     * @throws UnSupportedDatabaseException
+     */
+    public MSSQLTransaction(ConnectionDefinition def)
+            throws ClassNotFoundException, SQLException,
+            MissingConnectionParamException, UnSupportedDatabaseException {
+        super(def);
+    // TODO Auto-generated constructor stub
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see at.redeye.FrameWork.base.Transaction#markColumn(java.lang.String)
-	 */
-	@Override
-	public String markColumn(String in) {
-		return "[" + in.toLowerCase() + "]";
-	}
-
-	@Override
-	public String markTable(String in) {
-		return "[" + in.toUpperCase() + "]";
-	}
-
-	@Override
-	public String getDayStmt(String column, DBDateTime day) {
-
-		StringBuilder str = new StringBuilder();
-
-		str.append(markColumn(column));
-		str.append(" = '");
-		str.append(day.getDateStr());
-		str.append("'");
-
-		return str.toString();
-	}
-
-	@Override
-	public String getPeriodStmt(String column, DBDateTime begin, DBDateTime end) {
-
-		StringBuilder str = new StringBuilder();
-
-		str.append(markColumn(column));
-		str.append(" BETWEEN ");
-		str.append("'");
-		str.append(begin.toString());
-		str.append("'");
-		str.append(" AND ");
-		str.append("'");
-		str.append(end.toString());
-		str.append("'");
-
-		return str.toString();
-	}
-
-	@Override
-	public String getDayStmt(String column, String dayStr) {
-
-		StringBuilder str = new StringBuilder();
-
-		str.append(markColumn(column));
-		str.append(" = '");
-		str.append(dayStr);
-		str.append("'");
-
-		return str.toString();
-	}
-
-	@Override
-	public String getPeriodStmt(String column, String beginStr, String endStr) {
-
-		StringBuilder str = new StringBuilder();
-
-		str.append(markColumn(column));
-		str.append(" BETWEEN ");
-		str.append("'");
-		str.append(beginStr);
-		str.append("'");
-		str.append(" AND ");
-		str.append("'");
-		str.append(endStr);
-		str.append("'");
-
-		return str.toString();
-
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see at.redeye.FrameWork.base.Transaction#markColumn(java.lang.String)
+     */
+    @Override
+    public String markColumn(String in) {
+        return "[" + in.toLowerCase() + "]";
+    }
 
     @Override
-    public String getPeriodStmt(String column1, String column2, DateMidnight date) 
-    {
+    public String markTable(String in) {
+        return "[" + in.toUpperCase() + "]";
+    }
+
+    @Override
+    public String getDayStmt(String column, DBDateTime day) {
+
         StringBuilder str = new StringBuilder();
 
-        
-        String str_date = DBDateTime.getStdString(date);       
-        
-		str.append(markColumn(column1));
-		str.append(" >= '");
-		str.append( str_date );
-		str.append("' AND '");
-		str.append( str_date );
-		str.append("' < ");				
+        str.append(markColumn(column));
+        str.append(" = '");
+        str.append(day.getDateStr());
+        str.append("'");
+
+        return str.toString();
+    }
+
+    @Override
+    public String getPeriodStmt(String column, DBDateTime begin, DBDateTime end) {
+
+        StringBuilder str = new StringBuilder();
+
+        str.append(markColumn(column));
+        str.append(" BETWEEN ");
+        str.append("'");
+        str.append(begin.toString());
+        str.append("'");
+        str.append(" AND ");
+        str.append("'");
+        str.append(end.toString());
+        str.append("'");
+
+        return str.toString();
+    }
+
+    @Override
+    public String getDayStmt(String column, String dayStr) {
+
+        StringBuilder str = new StringBuilder();
+
+        str.append(markColumn(column));
+        str.append(" = '");
+        str.append(dayStr);
+        str.append("'");
+
+        return str.toString();
+    }
+
+    @Override
+    public String getPeriodStmt(String column, String beginStr, String endStr) {
+
+        StringBuilder str = new StringBuilder();
+
+        str.append(markColumn(column));
+        str.append(" BETWEEN ");
+        str.append("'");
+        str.append(beginStr);
+        str.append("'");
+        str.append(" AND ");
+        str.append("'");
+        str.append(endStr);
+        str.append("'");
+
+        return str.toString();
+
+    }
+
+    @Override
+    public String getPeriodStmt(String column1, String column2, DateMidnight date) {
+        StringBuilder str = new StringBuilder();
+
+
+        String str_date = DBDateTime.getStdString(date);
+
+        str.append(markColumn(column1));
+        str.append(" >= '");
+        str.append(str_date);
+        str.append("' AND '");
+        str.append(str_date);
+        str.append("' < ");
         str.append(markColumn(column2));
 
-		return str.toString();
+        return str.toString();
     }
-    
-	@Override
-	public String getDayStmt(String column, Date day) {
 
-		DateMidnight dm = new DateMidnight(day);
+    @Override
+    public String getDayStmt(String column, Date day) {
 
-		return getDayStmt(column, dm);
-	}
+        DateMidnight dm = new DateMidnight(day);
 
-	@Override
-	public String getDayStmt(String column, DateMidnight day) {
-		return getPeriodStmt(column, DBDateTime.getStdString(day), DBDateTime
-				.getStdString(day.plusDays(1)));
-	}
+        return getDayStmt(column, dm);
+    }
 
-	@Override
-	public String getPeriodStmt(String column, DateMidnight dm_from,
-			DateMidnight dm_to) {
-		return getPeriodStmt(column, DBDateTime.getStdString(dm_from),
-				DBDateTime.getStdString(dm_to));
-	}
+    @Override
+    public String getDayStmt(String column, DateMidnight day) {
+        return getPeriodStmt(column, DBDateTime.getStdString(day), DBDateTime.getStdString(day.plusDays(1)));
+    }
+
+    @Override
+    public String getPeriodStmt(String column, DateMidnight dm_from,
+            DateMidnight dm_to) {
+        return getPeriodStmt(column, DBDateTime.getStdString(dm_from),
+                DBDateTime.getStdString(dm_to));
+    }
 
     @Override
     public String getGUIFilterWhereStmt(Vector<? extends JComponent> fromFilter, Vector<? extends JComponent> toFilter) {
 
-        return "";
-    }
+        StringBuilder str = new StringBuilder();
+        boolean foundPartner;
 
+
+        if (fromFilter != null && fromFilter.size() > 0) {
+            str.append(" WHERE ");
+            String nameFrom = "";
+            String valueFrom = "";
+            for (int i = 0; i < fromFilter.size(); i++) {
+                foundPartner = false;
+                JComponent c = fromFilter.get(i);
+                if (c instanceof DBFilterComboBox) {
+                    nameFrom = ((DBFilterComboBox) c).getColName();
+                    valueFrom = ((DBFilterComboBox) c).getSelectedItem().toString();
+                } else if (c instanceof DBFilterEditField) {
+                    nameFrom = ((DBFilterComboBox) c).getColName();
+                    valueFrom = ((DBFilterComboBox) c).getSelectedItem().toString();
+                } else {
+                    // not supported item
+                    System.out.println("Unsupported filter element in container!");
+                    return "";
+
+                }
+                String nameTo = "";
+                String valueTo = "";
+                for (int j = 0; j < toFilter.size(); j++) {
+                    JComponent cTo = toFilter.get(j);
+                    if (c instanceof DBFilterComboBox) {
+                        nameTo = ((DBFilterComboBox) cTo).getColName();
+                        valueTo = ((DBFilterComboBox) cTo).getSelectedItem().toString();
+                    } else if (c instanceof DBFilterEditField) {
+                        nameTo = ((DBFilterComboBox) cTo).getColName();
+                        valueTo = ((DBFilterComboBox) cTo).getSelectedItem().toString();
+                    } else {
+                        // not supported item
+                        System.out.println("Unsupported filter element in container!");
+                        return "";
+
+                    }
+                    if (nameTo.equals(nameFrom)) {
+                        foundPartner = true;
+                        break;
+                    }
+
+                }
+
+                if (!foundPartner) {
+                    str.append(markColumn(nameFrom) + " = ");
+                    str.append("'" + valueFrom + "' ");
+                } else {
+                    str.append(markColumn(nameFrom) + " between ");
+                    str.append("'" + valueFrom + "' and ");
+                    str.append("'" + valueTo + "' ");
+                }
+
+                if (i < fromFilter.size() - 1) {
+                    str.append(" and ");
+                }
+
+            }
+        }
+        System.out.println("build: " + str.toString());
+        return str.toString();
+    }
 }
--- at/redeye/FrameWork/base/transaction/MySQLTransaction.java
+++ at/redeye/FrameWork/base/transaction/MySQLTransaction.java
@@ -6,6 +6,8 @@ package at.redeye.FrameWork.base.transaction;
 import java.sql.SQLException;
 
 import at.redeye.FrameWork.base.bindtypes.DBDateTime;
+import at.redeye.FrameWork.widgets.DBFilterComboBox;
+import at.redeye.FrameWork.widgets.DBFilterEditField;
 import at.redeye.SqlDBInterface.SqlDBConnection.impl.ConnectionDefinition;
 import at.redeye.SqlDBInterface.SqlDBConnection.impl.MissingConnectionParamException;
 import at.redeye.SqlDBInterface.SqlDBConnection.impl.UnSupportedDatabaseException;
@@ -21,175 +23,226 @@ import org.joda.time.DateMidnight;
  */
 public class MySQLTransaction extends Transaction {
 
-	/**
-	 * 
-	 */
-	protected MySQLTransaction() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     *
+     */
+    protected MySQLTransaction() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param def
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
-	 * @throws MissingConnectionParamException
-	 * @throws UnSupportedDatabaseException
-	 */
-	public MySQLTransaction(ConnectionDefinition def)
-			throws ClassNotFoundException, SQLException,
-			MissingConnectionParamException, UnSupportedDatabaseException {
-		super(def);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param def
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws MissingConnectionParamException
+     * @throws UnSupportedDatabaseException
+     */
+    public MySQLTransaction(ConnectionDefinition def)
+            throws ClassNotFoundException, SQLException,
+            MissingConnectionParamException, UnSupportedDatabaseException {
+        super(def);
+    // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public String markColumn(String in) {
-		return ("`" + in.toLowerCase() + "`");
-	}
+    @Override
+    public String markColumn(String in) {
+        return ("`" + in.toLowerCase() + "`");
+    }
 
-	@Override
-	public String markTable(String in) {
-		return ("`" + in.toUpperCase() + "`");
-	}
+    @Override
+    public String markTable(String in) {
+        return ("`" + in.toUpperCase() + "`");
+    }
 
-	@Override
-	public String getDayStmt(String column, DBDateTime day) {
+    @Override
+    public String getDayStmt(String column, DBDateTime day) {
 
         /*
-		StringBuilder str = new StringBuilder();
-
-        
-		str.append(markColumn(column));
-		str.append(" LIKE '");
-		str.append(day.getDateStr());
-		str.append("%'");
-        */
-        DateMidnight dm = new DateMidnight( (Date) day.getValue() );        
-        
-		return getPeriodExclStmt( column, dm.toDate(), dm.plusDays(1).toDate());
-	}
-
-	@Override
-	public String getPeriodStmt(String column, DBDateTime begin, DBDateTime end) {
-
-		StringBuilder str = new StringBuilder();
-
-		str.append(markColumn(column));
-		str.append(" >= '");
-		str.append(begin.toString());
-		str.append("%' AND ");
-		str.append(markColumn(column));
-		str.append(" <= '");
-		str.append(end.toString());
-		str.append("%'");
-
-		return str.toString();
-
-	}
-
-	@Override
-	public String getDayStmt(String column, String dayStr) {
-
-		StringBuilder str = new StringBuilder();
-		str.append(markColumn(column));
-		str.append(" LIKE '");
-		str.append(dayStr);
-		str.append("%'");
-
-		return str.toString();
-	}
-
-	@Override
-	public String getPeriodStmt(String column, String beginStr, String endStr) {
-
-		StringBuilder str = new StringBuilder();
-
-		str.append(markColumn(column));
-		str.append(" >= '");
-		str.append(beginStr);
-		str.append("' AND ");
-		str.append(markColumn(column));
-		str.append(" <= '");
-		str.append(endStr);
-		str.append("'");
-
-		return str.toString();
-
-	}
-
-    private String getPeriodExclStmt(String column, Date begin, Date end) {
-        
         StringBuilder str = new StringBuilder();
 
         
+        str.append(markColumn(column));
+        str.append(" LIKE '");
+        str.append(day.getDateStr());
+        str.append("%'");
+         */
+        DateMidnight dm = new DateMidnight((Date) day.getValue());
+
+        return getPeriodExclStmt(column, dm.toDate(), dm.plusDays(1).toDate());
+    }
+
+    @Override
+    public String getPeriodStmt(String column, DBDateTime begin, DBDateTime end) {
+
+        StringBuilder str = new StringBuilder();
+
+        str.append(markColumn(column));
+        str.append(" >= '");
+        str.append(begin.toString());
+        str.append("%' AND ");
+        str.append(markColumn(column));
+        str.append(" <= '");
+        str.append(end.toString());
+        str.append("%'");
+
+        return str.toString();
+
+    }
+
+    @Override
+    public String getDayStmt(String column, String dayStr) {
+
+        StringBuilder str = new StringBuilder();
+        str.append(markColumn(column));
+        str.append(" LIKE '");
+        str.append(dayStr);
+        str.append("%'");
+
+        return str.toString();
+    }
+
+    @Override
+    public String getPeriodStmt(String column, String beginStr, String endStr) {
+
+        StringBuilder str = new StringBuilder();
+
+        str.append(markColumn(column));
+        str.append(" >= '");
+        str.append(beginStr);
+        str.append("' AND ");
+        str.append(markColumn(column));
+        str.append(" <= '");
+        str.append(endStr);
+        str.append("'");
+
+        return str.toString();
+
+    }
+
+    private String getPeriodExclStmt(String column, Date begin, Date end) {
+
+        StringBuilder str = new StringBuilder();
+
+
         String str_begin = DBDateTime.getStdString(begin);
         String str_end = DBDateTime.getStdString(end);
-        
-		str.append(markColumn(column));
-		str.append(" >= '");
-		str.append( str_begin );
-		str.append("' AND ");
-		str.append(markColumn(column));
-		str.append(" < '");
-		str.append(str_end);
-		str.append("'");		
 
-		return str.toString();
+        str.append(markColumn(column));
+        str.append(" >= '");
+        str.append(str_begin);
+        str.append("' AND ");
+        str.append(markColumn(column));
+        str.append(" < '");
+        str.append(str_end);
+        str.append("'");
+
+        return str.toString();
     }
 
     @Override
     public String getDayStmt(String column, Date day) {
-        
-        DateMidnight dm = new DateMidnight( day );
-        
+
+        DateMidnight dm = new DateMidnight(day);
+
         return getDayStmt(column, dm);
     }
 
     @Override
     public String getDayStmt(String column, DateMidnight day) {
-        return getPeriodExclStmt( column, day.toDate(), day.plusDays(1).toDate());
+        return getPeriodExclStmt(column, day.toDate(), day.plusDays(1).toDate());
     }
 
     @Override
     public String getPeriodStmt(String column, DateMidnight dm_from, DateMidnight dm_to) {
-        return getPeriodStmt( column, 
-                              DBDateTime.getStdString(dm_from), 
-                              DBDateTime.getStdString(dm_to));
+        return getPeriodStmt(column,
+                DBDateTime.getStdString(dm_from),
+                DBDateTime.getStdString(dm_to));
     }
 
     @Override
-    public String getPeriodStmt(String column1, String column2, DateMidnight date) 
-    {
+    public String getPeriodStmt(String column1, String column2, DateMidnight date) {
         StringBuilder str = new StringBuilder();
 
-        
-        String str_date = DBDateTime.getStdString(date);       
-        
-		str.append(markColumn(column1));
-		str.append(" >= '");
-		str.append( str_date );
-		str.append("' AND '");
-        str.append(str_date);		
-		str.append("' < ");
-		str.append(markColumn(column2));		
 
-		return str.toString();
+        String str_date = DBDateTime.getStdString(date);
+
+        str.append(markColumn(column1));
+        str.append(" >= '");
+        str.append(str_date);
+        str.append("' AND '");
+        str.append(str_date);
+        str.append("' < ");
+        str.append(markColumn(column2));
+
+        return str.toString();
     }
 
     @Override
     public String getGUIFilterWhereStmt(Vector<? extends JComponent> fromFilter, Vector<? extends JComponent> toFilter) {
 
         StringBuilder str = new StringBuilder();
+        boolean foundPartner;
 
 
-        if (fromFilter != null || toFilter != null) {
-             str.append(" WHERE ");
-             for (int i = 0; i < fromFilter.size(); i++) {
-                 
-             }
+        if (fromFilter != null && fromFilter.size() > 0) {
+            str.append(" WHERE ");
+            String nameFrom;
+            String valueFrom;
+            for (int i = 0; i < fromFilter.size(); i++) {
+                foundPartner = false;
+                JComponent c = fromFilter.get(i);
+                if (c instanceof DBFilterComboBox) {
+                    nameFrom = ((DBFilterComboBox) c).getColName();
+                    valueFrom = ((DBFilterComboBox) c).getSelectedItem().toString();
+                } else if (c instanceof DBFilterEditField) {
+                    nameFrom = ((DBFilterComboBox) c).getColName();
+                    valueFrom = ((DBFilterComboBox) c).getSelectedItem().toString();
+                } else {
+                    // not supported item
+                    System.out.println("Unsupported filter element in container!");
+                    return "";
+
+                }
+                String nameTo = "";
+                String valueTo = "";
+                for (int j = 0; j < toFilter.size(); j++) {
+                    JComponent cTo = toFilter.get(j);
+                    if (c instanceof DBFilterComboBox) {
+                        nameTo = ((DBFilterComboBox) cTo).getColName();
+                        valueTo = ((DBFilterComboBox) cTo).getSelectedItem().toString();
+                    } else if (c instanceof DBFilterEditField) {
+                        nameTo = ((DBFilterComboBox) cTo).getColName();
+                        valueTo = ((DBFilterComboBox) cTo).getSelectedItem().toString();
+                    } else {
+                        // not supported item
+                        System.out.println("Unsupported filter element in container!");
+                        return "";
+
+                    }
+                    if (nameTo.equals(nameFrom)) {
+                        foundPartner = true;
+                        break;
+                    }
+
+                }
+
+                if (!foundPartner) {
+                    str.append (markColumn(nameFrom) + " = ");
+                    str.append("'"+valueFrom+"' ");
+                } else {
+                    str.append (markColumn(nameFrom) + " >= ");
+                    str.append ("'"+valueFrom+"' and ");
+                    str.append (markColumn(nameTo) + " <= ");
+                    str.append ("'"+valueTo+"' ");
+                }
+
+                if (i < fromFilter.size()-1) {
+                    str.append (" and ");
+                }
+
+            }
         }
-
-        return "";
+        System.out.println("build: "+str.toString());
+        return str.toString();
     }
-
 }
