b80dd71583f03cd93e00bbb87a3563d9b5c66660
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-06-06 15:19:25 +0000

    Fixed
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@294 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/Communication/sps/H1/test/TestServer.java
+++ at/redeye/Communication/sps/H1/test/TestServer.java
@@ -7,47 +7,50 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestServer {
 
-	public TestServer() {
-		// TODO Auto-generated constructor stub
-	}
+    public TestServer() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			ServerSocket sso = new ServerSocket(9001);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            ServerSocket sso = new ServerSocket(9001);
+            Socket client = sso.accept(); // ATT: OUGHT TO BE A THREAD!
 
-			while (true) {
+            DataInputStream dis = new DataInputStream(
+                    new BufferedInputStream(client.getInputStream()));
+            DataOutputStream out = new DataOutputStream(
+                    new BufferedOutputStream(client.getOutputStream()));
+            byte[] buffer = new byte[30];
+            while (true) {
 
-				Socket client = sso.accept(); // ATT: OUGHT TO BE A THREAD!
 
-				DataInputStream dis = new DataInputStream(
-						new BufferedInputStream(client.getInputStream()));
-				DataOutputStream out = new DataOutputStream(
-						new BufferedOutputStream(client.getOutputStream()));
+                int len = dis.read(buffer, 0, 30);
+                for (int i = 0; i < len; i++) {
+                    System.out.print(String.format("%02x ", (byte) buffer[i]));
+                }
+                buffer[5] = (byte) 0xd0; // CODE_CC vorgaukeln
 
-				byte[] buffer = new byte[512];
-				int len = dis.read(buffer, 0, 512);
-				for (int i = 0; i < len; i++) {
-					System.out.print(String.format("%02x ", (byte) buffer[i]));
-				}
-				
+                out.write(buffer);
+                out.flush();
 
-				buffer[5] = (byte) 0xd0; // CODE_CC vorgaukeln
-				
-				out.write(buffer);
-				out.flush();
+                Thread.sleep(500);
 
-			
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-	}
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 }
