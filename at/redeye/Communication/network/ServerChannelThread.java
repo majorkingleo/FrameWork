b3546c3832de726ae0d6e7c0e1d24144f4fe06d8
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-06-18 19:11:02 +0000

    Initially added to be able to handle non-master clients
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@299 0809fef9-18a3-0410-bae6-c9758b01f9bc

---
+++ at/redeye/Communication/network/ServerChannelThread.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package at.redeye.Communication.network;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+import at.redeye.Communication.sps.H1.comm.impl.H1SenderThread;
+
+/**
+ * @author Mario Mattl
+ * 
+ */
+public class ServerChannelThread extends Thread {
+
+	private int port;
+	private SynchronousQueue<byte []> squeue;
+
+	public ServerChannelThread(int port) {
+		super();
+		this.port = port;
+		this.squeue = H1SenderThread.getQueue();
+	}
+
+	public void run() {
+
+		try {
+			ServerSocketChannel ssc = ServerSocketChannel.open();
+			ServerSocket ss = ssc.socket();
+			java.nio.channels.Selector sel = java.nio.channels.Selector.open();
+
+			ss.bind(new InetSocketAddress(port));
+			ssc.configureBlocking(false);
+			ssc.register(sel, SelectionKey.OP_ACCEPT);
+
+			while (true) {
+				int n = sel.select();
+
+				if (n == 0)
+					continue;
+
+				Iterator<SelectionKey> it = sel.selectedKeys().iterator();
+
+				while (it.hasNext()) {
+					SelectionKey key = (SelectionKey) it.next();
+					// Is a new connection coming in?
+					if (key.isAcceptable()) {
+						ServerSocketChannel server = (ServerSocketChannel) key
+								.channel();
+						SocketChannel channel = server.accept();
+
+						registerChannel(sel, channel, SelectionKey.OP_READ);
+
+					}
+
+					// is there data to read on this channel?
+					if (key.isReadable()) {
+						queueDataFromSocket(key);
+					}
+
+					// remove key from selected set, it's been handled
+					it.remove();
+				}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+	// ----------------------------------------------------------
+
+	/**
+	 * Register the given channel with the given selector for the given
+	 * operations of interest
+	 * @throws IOException 
+	 */
+	protected void registerChannel(java.nio.channels.Selector selector,
+			SelectableChannel channel, int ops) throws IOException  {
+		
+		if (channel == null) {
+			return; // could happen
+		}
+
+		// set the new channel non-blocking
+		channel.configureBlocking(false);
+
+		// register it with the selector
+		channel.register(selector, ops);
+	}
+	
+	
+	   protected void queueDataFromSocket (SelectionKey key) throws IOException
+	      
+	   {
+	      SocketChannel socketChannel = (SocketChannel) key.channel();
+	      ByteBuffer buffer = ByteBuffer.allocate(100);
+	      int count;
+
+	      buffer.clear();         // make buffer empty
+
+	      // loop while data available, channel is non-blocking
+	      while ((count = socketChannel.read (buffer)) > 0) {
+	    	  
+	         buffer.flip();      // make buffer readable
+	         byte [] arr = buffer.array();
+	         for (int i = 0; i < arr.length; i++) {
+	        	 
+	        	 System.out.print(String.format("%02x ", arr[i]));
+	        	 
+	         }
+	         System.out.println("\n");
+	         
+	        
+	        try {
+				boolean res = squeue.offer(arr, 200, TimeUnit.MILLISECONDS);
+				if (res == false) {
+					System.out.println("OFFER FAILED!");
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	         buffer.clear();
+
+	      }
+
+	      if (count < 0) {
+	         // close channel on EOF, invalidates the key
+	         socketChannel.close();
+	      }
+	   }
+
+}
