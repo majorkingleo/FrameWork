a6b2cfb5de143220bc1bf6299314c23f1acb11a2
kingleo <kingleo@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-06-27 22:33:24 +0000

    Image Auswahlliste hinzugef��gt.
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@335 0809fef9-18a3-0410-bae6-c9758b01f9bc

---
+++ at/redeye/FrameWork/base/imagestorage/ImageSelectListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.redeye.FrameWork.base.imagestorage;
+
+import at.redeye.FrameWork.base.imagestorage.bindtypes.DBImage;
+
+/**
+ *
+ * @author martin
+ */
+public interface ImageSelectListener 
+{
+    public void ImageSelected( DBImage image );
+}
