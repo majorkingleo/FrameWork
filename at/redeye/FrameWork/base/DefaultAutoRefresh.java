a6c53524319db128e9f41474e97dd071586a1c76
kingleo <kingleo@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-07-10 17:09:43 +0000

    Weiterenwicklung
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@346 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/base/DefaultAutoRefresh.java
+++ at/redeye/FrameWork/base/DefaultAutoRefresh.java
@@ -5,15 +5,29 @@
 
 package at.redeye.FrameWork.base;
 
+import javax.swing.SwingUtilities;
+
 /**
- *
- * @author Sabrina und Mario
+ * 
+ * @author Mario Mattl
  */
 public class DefaultAutoRefresh {
 
-    public static void DefaultAutoRefreshTable(AutoRefreshInterface afif)
-    {
-        afif.feed_table(true);
-    }
+	public static void DefaultAutoRefreshTable(final AutoRefreshInterface afif) {
+		
+		
+		// This normally causes a re-read of table data, so we have to put
+		// the operation onto the EventQueue, because we don't want to disturb redraw events! 
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				afif.feed_table(true);
+
+			}
+
+		});
+
+	}
 
 }
