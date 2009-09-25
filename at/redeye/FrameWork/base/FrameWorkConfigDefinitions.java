7364964af17c409b12b9951f2bb309c6e8113fd8
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-09-25 20:13:46 +0000

    Added AutoLogin parameters
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@382 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/base/FrameWorkConfigDefinitions.java
+++ at/redeye/FrameWork/base/FrameWorkConfigDefinitions.java
@@ -2,39 +2,39 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package at.redeye.FrameWork.base;
 
 import at.redeye.FrameWork.base.bindtypes.DBConfig;
-
 
 /**
  *
  * @author martin
  */
 public class FrameWorkConfigDefinitions {
-    
-    public static DBConfig HelpParamColorEven = new DBConfig("HelpParamColorEven", "#fefeaa", "Hilfehintergundfarbe für Parameter gerade Zeilen." );
-    public static DBConfig HelpParamColorOdd = new DBConfig("HelpParamColorOdd", "#ddeedd", "Hilfehintergundfarbe für Parameter ungerade Zeilen." );
-    public static DBConfig HelpParamColorTitle = new DBConfig("HelpParamColorTitle", "#aaddff", "Hilfehintergundfarbe für Parameter Titel." );
-    public static DBConfig SpreadSheetColorEven = new DBConfig("SpreadSheetColorEven", "#d2ebf5", "Hintergundfarbe der Tabelle bei geraden Reihen." );
-    public static DBConfig SpreadSheetColorEvenEditable = new DBConfig("SpreadSheetColorEvenEditable", "#f5f5ff", "Hintergundfarbe der Tabelle bei geraden editiebaren Reihen." );
-    public static DBConfig SpreadSheetColorOdd = new DBConfig("SpreadSheetColorOdd", "#ffffff", "Hintergundfarbe der Tabelle bei ungeraden Reihen." );
-    public static DBConfig SpreadSheetColorOddEditable = new DBConfig("SpreadSheetColorOddEditable", "#dcf5eb", "Hintergundfarbe der Tabelle bei ungeraden editiebaren Reihen." );
-    public static DBConfig SpreadSheetMarginEditable = new DBConfig("SpreadSheetMarginEditable","20","Zusätzlicher Rand, um den die Tabellenspalten breiter gemacht werden um besseres Editieren zu ermöglichen. Dies gilt nur für editierbare Spalten.");
-    public static DBConfig SpreadSheetMarginReadOnly = new DBConfig("SpreadSheetMarginReadOnly","5","Zusätzlicher Rand, um den die Tabellenspalten breiter gemacht werden. Dieser Wert gilt nur für nicht editierbare Spalten.");
-    public static DBConfig DefaultAutoLineBreakWidth = new DBConfig( "DefaultAutoLineBreakWidth", "40", "Breite eines automatisch umgebrochenen Textes.");
-    public static DBConfig ImagePreviewInFileOpen = new DBConfig("ImagePreviewinFileOpen","false","Soll im Datei öffnen Dialogen die Bildervorschau angezeigt werden?");
 
-    public static void registerDefinitions()
-    {
+    public static DBConfig HelpParamColorEven = new DBConfig("HelpParamColorEven", "#fefeaa", "Hilfehintergundfarbe für Parameter gerade Zeilen.");
+    public static DBConfig HelpParamColorOdd = new DBConfig("HelpParamColorOdd", "#ddeedd", "Hilfehintergundfarbe für Parameter ungerade Zeilen.");
+    public static DBConfig HelpParamColorTitle = new DBConfig("HelpParamColorTitle", "#aaddff", "Hilfehintergundfarbe für Parameter Titel.");
+    public static DBConfig SpreadSheetColorEven = new DBConfig("SpreadSheetColorEven", "#d2ebf5", "Hintergundfarbe der Tabelle bei geraden Reihen.");
+    public static DBConfig SpreadSheetColorEvenEditable = new DBConfig("SpreadSheetColorEvenEditable", "#f5f5ff", "Hintergundfarbe der Tabelle bei geraden editiebaren Reihen.");
+    public static DBConfig SpreadSheetColorOdd = new DBConfig("SpreadSheetColorOdd", "#ffffff", "Hintergundfarbe der Tabelle bei ungeraden Reihen.");
+    public static DBConfig SpreadSheetColorOddEditable = new DBConfig("SpreadSheetColorOddEditable", "#dcf5eb", "Hintergundfarbe der Tabelle bei ungeraden editiebaren Reihen.");
+    public static DBConfig SpreadSheetMarginEditable = new DBConfig("SpreadSheetMarginEditable", "20", "Zusätzlicher Rand, um den die Tabellenspalten breiter gemacht werden um besseres Editieren zu ermöglichen. Dies gilt nur für editierbare Spalten.");
+    public static DBConfig SpreadSheetMarginReadOnly = new DBConfig("SpreadSheetMarginReadOnly", "5", "Zusätzlicher Rand, um den die Tabellenspalten breiter gemacht werden. Dieser Wert gilt nur für nicht editierbare Spalten.");
+    public static DBConfig DefaultAutoLineBreakWidth = new DBConfig("DefaultAutoLineBreakWidth", "40", "Breite eines automatisch umgebrochenen Textes.");
+    public static DBConfig ImagePreviewInFileOpen = new DBConfig("ImagePreviewinFileOpen", "false", "Soll im Datei öffnen Dialogen die Bildervorschau angezeigt werden?");
+    public static DBConfig AllowAutoLogin = new DBConfig("AllowAutoLogin", "false", "Ist eine automatische Anmeldung generell zulässig?");
+    public static DBConfig AutoLoginUser = new DBConfig("AutoLoginUser", "", "Legt den Login fest, mit dem die automatische Anmeldung durchgeführt wird.");
+    
+
+    public static void registerDefinitions() {
         GlobalConfigDefinitions.add_help_path("/at/momm/FrameWork/framework/resources/Help/Params/");
-        LocalConfigDefinitions.add_help_path("/at/momm/FrameWork/framework/resources/Help/Params/");        
-        
+        LocalConfigDefinitions.add_help_path("/at/momm/FrameWork/framework/resources/Help/Params/");
+
         addLocal(HelpParamColorEven);
         addLocal(HelpParamColorOdd);
         addLocal(HelpParamColorTitle);
-        
+
         addLocal(SpreadSheetColorEven);
         addLocal(SpreadSheetColorEvenEditable);
         addLocal(SpreadSheetColorOdd);
@@ -44,21 +44,22 @@ public class FrameWorkConfigDefinitions {
 
         addLocal(DefaultAutoLineBreakWidth);
         addLocal(ImagePreviewInFileOpen);
-    }
-    
-    static void add( String name, String value, String descr )
-    {
-        GlobalConfigDefinitions.add(new DBConfig(name,value,descr));
-    }
-    
-    static void add( DBConfig c )
-    {
-        GlobalConfigDefinitions.add(c);
-    }        
 
-    
-    static void addLocal( DBConfig c )
-    {
+        addLocal(AutoLoginUser);
+        
+
+        add(AllowAutoLogin);
+    }
+
+    static void add(String name, String value, String descr) {
+        GlobalConfigDefinitions.add(new DBConfig(name, value, descr));
+    }
+
+    static void add(DBConfig c) {
+        GlobalConfigDefinitions.add(c);
+    }
+
+    static void addLocal(DBConfig c) {
         LocalConfigDefinitions.add(c);
-    }  
+    }
 }
