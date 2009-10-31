15ad91ae3f826203e9746f0b20c96b3db5f74268
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-10-31 16:53:58 +0000

    Increased search window for blank-seeking: Large word compositions, e.g. XXXXXXXXXXXXXX=>YYYYYYYYYYY caused that the splitter failed to find a suitable blank at wanted break point. Prettied up split behaviour in general
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@400 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/utilities/StringUtils.java
+++ at/redeye/FrameWork/utilities/StringUtils.java
@@ -12,227 +12,232 @@ import java.util.Vector;
  */
 public class StringUtils {
 
-	private static int defaultAutoLineLength = 40;
+    private static int defaultAutoLineLength = 40;
 
-	static boolean contains(char c, String what) {
-		return what.indexOf(c) >= 0;
-	}
+    static boolean contains(char c, String what) {
+        return what.indexOf(c) >= 0;
+    }
 
-	static int skip_char(StringBuilder s, String what, int pos) {
-		while (pos <= (s.length() - 1)) {
-			char c;
+    static int skip_char(StringBuilder s, String what, int pos) {
+        while (pos <= (s.length() - 1)) {
+            char c;
 
-			c = s.charAt(pos);
+            c = s.charAt(pos);
 
-			if (contains(c, what)) {
-				pos++;
-				continue;
-			}
+            if (contains(c, what)) {
+                pos++;
+                continue;
+            }
 
-			break;
-		}
+            break;
+        }
 
-		return pos;
-	}
+        return pos;
+    }
 
-	static int skip_char_reverse(StringBuilder s, String what, int pos) {
-		while (pos > 0) {
-			char c;
+    static int skip_char_reverse(StringBuilder s, String what, int pos) {
+        while (pos > 0) {
+            char c;
 
-			c = s.charAt(pos);
+            c = s.charAt(pos);
 
-			if (contains(c, what)) {
-				pos--;
-				continue;
-			}
+            if (contains(c, what)) {
+                pos--;
+                continue;
+            }
 
-			break;
-		}
+            break;
+        }
 
-		return pos;
-	}
+        return pos;
+    }
 
-	public static int skip_spaces_reverse(StringBuilder s, int pos) {
-		return skip_char_reverse(s, " \t\n\r", pos);
-	}
+    public static int skip_spaces_reverse(StringBuilder s, int pos) {
+        return skip_char_reverse(s, " \t\n\r", pos);
+    }
 
-	public static int skip_spaces(StringBuilder s, int pos) {
-		return skip_char(s, " \t\n\r", pos);
-	}
+    public static int skip_spaces(StringBuilder s, int pos) {
+        return skip_char(s, " \t\n\r", pos);
+    }
 
-	public static boolean is_space(char c) {
-		if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
-			return true;
-		}
+    public static boolean is_space(char c) {
+        if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public static Vector<String> split_str(StringBuilder s, String c) {
-		Vector<String> res = new Vector<String>();
+    public static Vector<String> split_str(StringBuilder s, String c) {
+        Vector<String> res = new Vector<String>();
 
-		int start = 0;
-		do {
-			int pos = s.indexOf(c, start);
+        int start = 0;
+        do {
+            int pos = s.indexOf(c, start);
 
-			if (pos < 0) {
-				res.add(s.substring(start));
-				break;
-			}
+            if (pos < 0) {
+                res.add(s.substring(start));
+                break;
+            }
 
-			res.add(s.substring(start, pos));
+            res.add(s.substring(start, pos));
 
-			start = pos + 1;
+            start = pos + 1;
 
-		} while (start > 0);
+        } while (start > 0);
 
-		return res;
-	}
+        return res;
+    }
 
-	public static String strip(StringBuilder s, String what) {
-		int start = skip_char(s, what, 0);
-		int end = skip_char_reverse(s, what, s.length() - 1);
+    public static String strip(StringBuilder s, String what) {
+        int start = skip_char(s, what, 0);
+        int end = skip_char_reverse(s, what, s.length() - 1);
 
-		return s.substring(start, end + 1);
-	}
+        return s.substring(start, end + 1);
+    }
 
-	public static String strip(String s, String what) {
-		return strip(new StringBuilder(s), what);
-	}
-    
-    public static void set_defaultAutoLineLenght( int length )
-    {
+    public static String strip(String s, String what) {
+        return strip(new StringBuilder(s), what);
+    }
+
+    public static void set_defaultAutoLineLenght(int length) {
         defaultAutoLineLength = length;
     }
-    
-    public static int get_defaultAutoLineLenght()
-    {
+
+    public static int get_defaultAutoLineLenght() {
         return defaultAutoLineLength;
     }
 
-	public static String autoLineBreak(String what) {
-		return autoLineBreak(what, defaultAutoLineLength);
-	}
+    public static String autoLineBreak(String what) {
+        return autoLineBreak(what, defaultAutoLineLength);
+    }
 
-	public static String autoLineBreak(StringBuilder what, int length) {
-		return autoLineBreak(what.toString(), length);
-	}
+    public static String autoLineBreak(StringBuilder what, int length) {
+        return autoLineBreak(what.toString(), length);
+    }
 
-	public static String autoLineBreak(StringBuilder what) {
-		return autoLineBreak(what.toString(), defaultAutoLineLength);
-	}
+    public static String autoLineBreak(StringBuilder what) {
+        return autoLineBreak(what.toString(), defaultAutoLineLength);
+    }
 
-	public static String autoLineBreak(String what, int length) {
+    public static String autoLineBreak(String what, int length) {
 
-		final char[] myPreferedSigns = { ';', '.', ',', '!', '?' };
-		final char[] mySpaceSigns = { ' ', '\t' };
+        final char[] myPreferedSigns = {';', '.', ',', '!', '?', '-'};
+        final char[] mySpaceSigns = {' ', '\t'};
 
-		final int searchWindowLength = 20;
+        final int searchWindowLengthPreferedSigns = 20;
+        final int searchWindowLengthSpaceSigns = 50;
 
-		if (length < searchWindowLength / 2 || length >= what.length()
-				|| searchWindowLength / 2 >= what.length()) {
-			// Doesn't make sense
-			return what;
-		}
+        if (length < searchWindowLengthPreferedSigns / 2 ||
+                length >= what.length() ||
+                searchWindowLengthPreferedSigns / 2 >= what.length()) {
+            // Doesn't make sense
+            return what;
+        }
+        
+        char[] in = what.toCharArray();
+        StringBuilder str = new StringBuilder();
 
-		char[] in = what.toCharArray();
-		StringBuilder str = new StringBuilder();
+        for (int walker = 1; walker < in.length; walker++) {
 
-		for (int walker = 1; walker < in.length; walker++) {
+            if (in.length <= searchWindowLengthPreferedSigns / 2) {
+                break;
+            }
 
-			if (in.length <= searchWindowLength / 2) {
-				break;
-			}
+            if (walker % length == 0) {
 
-			if (walker % length == 0) {
+            //   System.out.println("Want to break at: " +
+            //          in[walker - 2] + in[walker - 1] + ">" +
+            //          in[walker] + "<" + in[walker + 1] + in[walker + 2]);
 
-				// try to find a sign in search window
-				boolean found = false;
-				for (int signidx = 0; signidx < myPreferedSigns.length; signidx++) {
+                // try to find a sign in search window
+                boolean found = false;
+                for (int signidx = 0; signidx < myPreferedSigns.length; signidx++) {
 
-					// try with preferred signs
-					for (int index = 1; index <= (searchWindowLength / 2); index++) {
+                    // try with preferred signs
+                    for (int index = 1;
+                            index <= (searchWindowLengthPreferedSigns / 2); index++) {
 
-						if ((walker + index + 1) >= in.length
-								|| (walker - index) <= 0) {
-							break; // not enough left
-						}
+                        if ((walker + index + 1) >= in.length || (walker - index) <= 0) {
+                            break; // not enough left
+                        }
 
-						if (in[walker + index] == myPreferedSigns[signidx]) {
-							str.append(new String(in, 0, walker + index + 1));
-							str.append("\n");
-							walker += 2; // jump over break sign
+                        if (in[walker + index] == myPreferedSigns[signidx]) {
+                            str.append(new String(in, 0, walker + index + 1));
+                            str.append("\n");
+                            walker += 2; // jump over break sign
 
-							String rest = new String(in, walker + index,
-									in.length - (walker + index));
-							in = rest.toCharArray();
-							walker = 0;
-							found = true;
-							break;
-						} else if (in[walker - index] == myPreferedSigns[signidx]) {
-							str.append(new String(in, 0, walker - index + 1));
-							str.append("\n");
-							walker += 2; // jump over break sign
+                            String rest = new String(in, walker + index,
+                                    in.length - (walker + index));
+                            in = rest.toCharArray();
+                            walker = 0;
+                            found = true;
+                            break;
+                        } else if (in[walker - index] == myPreferedSigns[signidx]) {
+                            str.append(new String(in, 0, walker - index + 1));
+                            str.append("\n");
+                            walker += 2; // jump over break sign
 
-							String rest = new String(in, walker - index,
-									in.length - (walker - index));
-							in = rest.toCharArray();
+                            String rest = new String(in, walker - index,
+                                    in.length - (walker - index));
+                            in = rest.toCharArray();
 
-							walker = 0;
-							found = true;
-							break;
-						}
-					}
+                            walker = 0;
+                            found = true;
+                            break;
+                        }
+                    }
 
-					if (found)
-						break;
-				}
+                    if (found) {
+                        break;
+                    }
+                }
 
-				for (int signidx = 0; signidx < mySpaceSigns.length; signidx++) {
-					// try with blanks
-					for (int index = 1; index <= (searchWindowLength / 2); index++) {
+                for (int signidx = 0; signidx < mySpaceSigns.length; signidx++) {
+                    // try with blanks
+                    for (int index = 1;
+                            index <= (searchWindowLengthSpaceSigns / 2); index++) {
 
-						if ((walker + index + 1) >= in.length
-								|| (walker - index) <= 0) {
-							break; // not enough left
-						}
+                        if ((walker + index + 1) >= in.length || (walker - index) <= 0) {
+                            break; // not enough left
+                        }
 
-						if (in[walker + index] == mySpaceSigns[signidx]) {
+                        if (in[walker + index] == mySpaceSigns[signidx]) {
 
-							str.append(new String(in, 0, walker + index));
-							str.append("\n");
-							walker++;
-							String rest = new String(in, walker + index,
-									in.length - (walker + index));
-							in = rest.toCharArray();
-							walker = 0;
-							found = true;
-							break;
-						} else if (in[walker - index] == mySpaceSigns[signidx]) {
+                            str.append(new String(in, 0, walker + index));
+                            str.append("\n");
+                            walker++;
+                            String rest = new String(in, walker + index,
+                                    in.length - (walker + index));
+                            in = rest.toCharArray();
+                            walker = 0;
+                            found = true;
+                            break;
+                        } else if (in[walker - index] == mySpaceSigns[signidx]) {
 
-							str.append(new String(in, 0, walker - index + 1));
-							str.append("\n");
-							walker++;
-							String rest = new String(in, walker - index,
-									in.length - (walker - index));
-							in = rest.toCharArray();
-							walker = 0;
-							found = true;
-							break;
-						}
+                            str.append(new String(in, 0, walker - index + 1));
+                            str.append("\n");
+                            walker++;
+                            String rest = new String(in, walker - index,
+                                    in.length - (walker - index));
+                            in = rest.toCharArray();
+                            walker = 0;
+                            found = true;
+                            break;
+                        }
 
-					}
+                    }
 
-					if (found)
-						break;
+                    if (found) {
+                        break;
+                    }
 
-				}
+                }
 
-			}
+            }
 
-		}
-		str.append(in); // rest
-		return str.toString();
-	}
-
+        }
+        str.append(in); // rest
+        return str.toString();
+    }
 }
