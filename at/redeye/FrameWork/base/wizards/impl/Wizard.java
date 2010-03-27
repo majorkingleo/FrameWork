f72d159fff336ae89f5fe33899290bad2468cea0
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2010-03-27 13:29:12 +0000

    Red Eye Labs Wizard (not fully finished yet)
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@454 0809fef9-18a3-0410-bae6-c9758b01f9bc

---
+++ at/redeye/FrameWork/base/wizards/impl/Wizard.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.redeye.FrameWork.base.wizards.impl;
+
+import at.redeye.FrameWork.base.wizards.WizardAction;
+import at.redeye.FrameWork.base.wizards.WizardClientActionInterface;
+import at.redeye.FrameWork.base.wizards.WizardProperties;
+import java.util.Vector;
+
+/**
+ *
+ * @author Mario
+ *
+ */
+public class Wizard implements WizardClientActionInterface {
+
+    private Vector<WizardBaseWindow> allWindows = new Vector<WizardBaseWindow>();
+    private WizardProperties props = null;
+    int currentWindow = 0;
+
+    public Wizard(WizardProperties props) {
+        this.props = props;
+    }
+
+    public void start() {
+        if (allWindows.size() > 0) {
+            WizardBaseWindow window = allWindows.get(0);
+
+            window.controlButtons();
+            prepareLegendText(window);
+            window.onInit();
+        }
+    }
+
+    public void addWindow(WizardBaseWindow window) {
+
+        delegateProps(window);
+        allWindows.add(window);
+    }
+
+    private void delegateProps(WizardBaseWindow window) {
+
+        window.setButtonCancelText(props.getButtonCancelText());
+        window.setButtonFinishText(props.getButtonJumpEndText());
+        window.setButtonNextText(props.getButtonNextText());
+        window.setButtonPrevText(props.getButtonPrevText());
+        window.setAreaMenuTreeTitle(props.getLegendAreaTitle());
+
+
+    }
+
+    protected void handleUpdate() {
+        WizardBaseWindow window = allWindows.get(currentWindow);
+        switch (window.getRecentAction()) {
+            case WIZARD_ACTION_NEXT:
+                window.onClose();
+                currentWindow++;
+                window = allWindows.get(currentWindow);
+                prepareLegendText(window);
+                if (currentWindow == (allWindows.size() - 1)) {
+                    applyAction(WizardAction.WIZARD_ACTION_NEXT, false);
+                }
+                window.controlButtons();
+                window.onInit();
+
+                break;
+            case WIZARD_ACTION_PREV:
+                window.onClose();
+                currentWindow--;
+                window = allWindows.get(currentWindow);
+                if (currentWindow == 0) {
+                    applyAction(WizardAction.WIZARD_ACTION_PREV, false);
+                }
+                prepareLegendText(window);
+                window.controlButtons();
+                window.onInit();
+                break;
+
+
+        }
+    }
+
+    private void prepareLegendText(WizardBaseWindow window) {
+        StringBuilder legend = new StringBuilder();
+        for (int idx = 0; idx < allWindows.size(); idx++) {
+
+            if (idx == currentWindow) {
+                legend.append("=> ");
+            } else {
+                legend.append("   ");
+            }
+
+            legend.append((idx + 1) + ". "
+                    + allWindows.get(idx).getTitle() + "\n");
+
+        }
+        for (int idx = 0; idx < allWindows.size(); idx++) {
+            window = allWindows.get(idx);
+            window.textAreaMenuTree.setText(legend.toString());
+
+        }
+    }
+
+    public void applyAction(WizardAction wizardAction, boolean isGranted) {
+        WizardBaseWindow window = allWindows.get(currentWindow);
+        window.applyActionRule(wizardAction, isGranted);
+        window.controlButtons();
+    }
+}
