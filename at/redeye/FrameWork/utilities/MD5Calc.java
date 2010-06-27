aff15d295b466bbc729f861fba36f12507b8e757
kingleo <kingleo@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2010-06-27 20:02:03 +0000

    MD5 Summenberechnug f��r eine ganze Datei eingebaut.
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@563 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/utilities/MD5Calc.java
+++ at/redeye/FrameWork/utilities/MD5Calc.java
@@ -1,4 +1,9 @@
 package at.redeye.FrameWork.utilities;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -13,6 +18,14 @@ public class MD5Calc {
 	private MessageDigest md5;
 	private byte[] digest;
 
+	public MD5Calc() {
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException nsae) {
+			nsae.printStackTrace();
+		}
+	}
+
 	public MD5Calc(String algorithm) {
 		try {
 			md5 = MessageDigest.getInstance(algorithm);
@@ -21,7 +34,7 @@ public class MD5Calc {
 		}
 	}
 
-	public String toHexString(byte b) {
+	public static String toHexString(byte b) {
 		int value = (b & 0x7F) + (b < 0 ? 128 : 0);
 
 		String ret = (value < 16 ? "0" : "");
@@ -56,5 +69,58 @@ public class MD5Calc {
 
 		return strbuf.toString();
 	}
-	
+
+        /**
+         * converts byte values to a hey string
+         * @param data
+         * @return
+         */
+        public static String bytes2hex( byte [] data )
+        {
+            StringBuffer strbuf = new StringBuffer();
+
+            for (int i = 0; i < data.length; i++) {
+                strbuf.append(toHexString(data[i]));
+            }
+
+            return strbuf.toString();
+        }
+
+        /**
+         * Calcs Checksum for a file
+         * @param file
+         * @return MD5 String Hex coded
+         */
+    public String calcCheckSum(File file) throws FileNotFoundException, IOException {
+
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+
+        byte[] buffer = new byte[2048];
+
+        int numRead;
+        do {
+            numRead = in.read(buffer);
+            if (numRead > 0) {
+                md5.update(buffer, 0, numRead);
+            }
+        } while (numRead != -1);
+
+        in.close();
+
+        return bytes2hex(md5.digest() ).toLowerCase();
+    }
+
+
+    public static void main( String argv[] )
+    {
+        MD5Calc calc = new MD5Calc("MD5");
+
+            try
+            {
+                System.out.println("md5: " + calc.calcCheckSum(new File("/home/martin/hmcp.zip")) );
+            } catch( IOException ex ) {
+                System.out.println(ex);
+            }
+        
+    }
 }
