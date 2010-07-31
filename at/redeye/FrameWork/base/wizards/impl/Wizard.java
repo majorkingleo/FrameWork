58f76971c12858faab00d1e605f435e5c04b9fbe
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2010-07-31 10:18:55 +0000

    Using ArrayList now
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@599 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/base/wizards/impl/Wizard.java
+++ at/redeye/FrameWork/base/wizards/impl/Wizard.java
@@ -5,15 +5,16 @@
 package at.redeye.FrameWork.base.wizards.impl;
 
 import java.awt.Rectangle;
-import java.util.Vector;
 
 import at.redeye.FrameWork.base.wizards.WizardAction;
 import at.redeye.FrameWork.base.wizards.WizardAttachInterface;
 import at.redeye.FrameWork.base.wizards.WizardClientActionInterface;
 import at.redeye.FrameWork.base.wizards.WizardProperties;
 import at.redeye.FrameWork.base.wizards.impl.WizardListener.WizardStatus;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 
@@ -21,194 +22,187 @@ import java.util.LinkedList;
  * 
  */
 public class Wizard implements WizardClientActionInterface,
-		WizardAttachInterface {
+        WizardAttachInterface {
 
-	private Collection<WizardListener> allWizardListeners = new LinkedList<WizardListener>();
-	private Vector<WizardBaseWindow> allWindows = new Vector<WizardBaseWindow>();
-	private WizardProperties props = null;
+    private Collection<WizardListener> allWizardListeners = new LinkedList<WizardListener>();
+    private List<WizardBaseWindow> allWindows = new ArrayList<WizardBaseWindow>();
+    private WizardProperties props = null;
+    private boolean beeing_in_update_state = false;
+    private List<WizardListener> toRemoveListeners = new ArrayList<WizardListener>();
 
-        private boolean beeing_in_update_state = false;
-        private Vector<WizardListener> toRemoveListeners = new Vector<WizardListener>();;
+    ;
+    int currentWindow = 0;
 
-	int currentWindow = 0;
+    public Wizard(WizardProperties props) {
+        this.props = props;
+    }
 
-	public Wizard(WizardProperties props) {
-		this.props = props;
-	}
+    public void start() {
+        if (allWindows.size() > 0) {
+            WizardBaseWindow window = allWindows.get(0);
 
-	public void start() {
-		if (allWindows.size() > 0) {
-			WizardBaseWindow window = allWindows.get(0);
+            window.controlButtons();
+            prepareLegendText(window);
+            window.onInit();
+        }
+    }
 
-			window.controlButtons();
-			prepareLegendText(window);
-			window.onInit();
-		}
-	}
+    public void addWindow(WizardBaseWindow window) {
 
-	public void addWindow(WizardBaseWindow window) {
+        delegateProps(window);
+        allWindows.add(window);
+    }
 
-		delegateProps(window);
-		allWindows.add(window);
-	}
+    private void delegateProps(WizardBaseWindow window) {
 
-	private void delegateProps(WizardBaseWindow window) {
+        window.setButtonCancelText(props.getButtonCancelText());
+        window.setButtonFinishText(props.getButtonJumpEndText());
+        window.setButtonNextText(props.getButtonNextText());
+        window.setButtonPrevText(props.getButtonPrevText());
+        window.setAreaMenuTreeTitle(props.getLegendAreaTitle());
 
-		window.setButtonCancelText(props.getButtonCancelText());
-		window.setButtonFinishText(props.getButtonJumpEndText());
-		window.setButtonNextText(props.getButtonNextText());
-		window.setButtonPrevText(props.getButtonPrevText());
-		window.setAreaMenuTreeTitle(props.getLegendAreaTitle());
+    }
 
-	}
+    public void handleUpdate() {
 
-	public void handleUpdate() {
+        WizardBaseWindow window = allWindows.get(currentWindow);
+        Rectangle bounds = window.getBounds();
 
-		WizardBaseWindow window = allWindows.get(currentWindow);
-		Rectangle bounds = window.getBounds();
+        WizardAction action = window.getRecentAction();
 
-                WizardAction action = window.getRecentAction();
+        switch (action) {
 
-		switch (action) {
+            case WIZARD_ACTION_NEXT:
 
-		case WIZARD_ACTION_NEXT:
+                do {
 
-                    do {
+                    window.onClose(action);
+                    currentWindow++;
+                    window = allWindows.get(currentWindow);
+                    if (currentWindow == (allWindows.size() - 1)) {
+                        applyAction(WizardAction.WIZARD_ACTION_NEXT, false);
+                    }
+                    window.setBounds(bounds);
+                    prepareLegendText(window);
+                    window.controlButtons();
+                    window.onInit();
+                    window.toFront();
 
-			window.onClose(action);
-			currentWindow++;
-			window = allWindows.get(currentWindow);
-			if (currentWindow == (allWindows.size() - 1)) {
-				applyAction(WizardAction.WIZARD_ACTION_NEXT, false);
-			}
-			window.setBounds(bounds);
-			prepareLegendText(window);
-			window.controlButtons();
-			window.onInit();
-                        window.toFront();
+                } while (window.skipThisStep());
 
-                    } while( window.skipThisStep() );
+                break;
 
-			break;
-			
-		case WIZARD_ACTION_PREV:
+            case WIZARD_ACTION_PREV:
 
-                    do {
-			
-			window.onClose(action);
-			currentWindow--;
-			window = allWindows.get(currentWindow);
-			if (currentWindow == 0) {
-				applyAction(WizardAction.WIZARD_ACTION_PREV, false);
-			}
-			window.setBounds(bounds);
-			prepareLegendText(window);
-			window.controlButtons();
-			window.onInit();
-                        window.toFront();
+                do {
 
-                    } while( window.skipThisStep() );
-                    break;
-			
-		case WIZARD_ACTION_FINISH: 
+                    window.onClose(action);
+                    currentWindow--;
+                    window = allWindows.get(currentWindow);
+                    if (currentWindow == 0) {
+                        applyAction(WizardAction.WIZARD_ACTION_PREV, false);
+                    }
+                    window.setBounds(bounds);
+                    prepareLegendText(window);
+                    window.controlButtons();
+                    window.onInit();
+                    window.toFront();
 
-                        setWizardStatus(WizardStatus.CLOSED);
-			window.onClose(action);
-			break;
-			
-		case WIZARD_ACTION_CLOSE: 
+                } while (window.skipThisStep());
+                break;
 
-                        setWizardStatus(WizardStatus.CLOSED);
-			window.onClose(action);
-			break;
+            case WIZARD_ACTION_FINISH:
 
-		}
-		
-		
-	}
+                setWizardStatus(WizardStatus.CLOSED);
+                window.onClose(action);
+                break;
 
-	private void prepareLegendText(WizardBaseWindow window) {
+            case WIZARD_ACTION_CLOSE:
 
-		StringBuilder legend = new StringBuilder();
-		for (int idx = 0; idx < allWindows.size(); idx++) {
+                setWizardStatus(WizardStatus.CLOSED);
+                window.onClose(action);
+                break;
 
-			if (idx == currentWindow) {
-				legend.append("=> ");
-			} else {
-				legend.append("   ");
-			}
+        }
 
-			legend.append((idx + 1) + ". " + allWindows.get(idx).getTitle()
-					+ "\n");
 
-		}
-		for (int idx = 0; idx < allWindows.size(); idx++) {
-			window = allWindows.get(idx);
-			window.textAreaMenuTree.setText(legend.toString());
+    }
 
-		}
-	}
-	
-	
-	public void setWizardStatus(WizardStatus currentWizardStatus) {
-		updateWizardListeners(currentWizardStatus);	
-	}
-	
-	@Override
-	public void applyAction(WizardAction wizardAction, boolean isGranted) {
+    private void prepareLegendText(WizardBaseWindow window) {
 
-		WizardBaseWindow window = allWindows.get(currentWindow);
-		window.applyActionRule(wizardAction, isGranted);
-		window.controlButtons();
-	}
+        StringBuilder legend = new StringBuilder();
+        for (int idx = 0; idx < allWindows.size(); idx++) {
 
-	@Override
-	public void addWizardListener(WizardListener listener) {
-            if( listener != null )
-		allWizardListeners.add(listener);
-	}
-
-	@Override
-	public void removeWizardListener(WizardListener listener) {
-                        
-            if( !beeing_in_update_state )
-            {
-                allWizardListeners.remove(listener);
+            if (idx == currentWindow) {
+                legend.append("=> ");
+            } else {
+                legend.append("   ");
             }
-            else
-            {
-                toRemoveListeners.add(listener);
-            }
-	}
 
-	@Override
-	public void updateWizardListeners(WizardStatus currentWizardStatus) {
+            legend.append(idx + 1).append(". ").append(allWindows.get(idx).getTitle()).append("\n");
 
-            beeing_in_update_state = true;
+        }
+        for (int idx = 0; idx < allWindows.size(); idx++) {
+            window = allWindows.get(idx);
+            window.textAreaMenuTree.setText(legend.toString());
 
-            System.out.println("UPDATE WIZARD LISTENER");          
+        }
+    }
 
-            for (WizardListener currentListener : allWizardListeners) {
-                if( currentListener.onStateChange(currentWizardStatus) == false )
-                {
-                    if( !toRemoveListeners.contains(currentListener) )
-                        toRemoveListeners.add(currentListener);
+    public void setWizardStatus(WizardStatus currentWizardStatus) {
+        updateWizardListeners(currentWizardStatus);
+    }
+
+    @Override
+    public void applyAction(WizardAction wizardAction, boolean isGranted) {
+
+        WizardBaseWindow window = allWindows.get(currentWindow);
+        window.applyActionRule(wizardAction, isGranted);
+        window.controlButtons();
+    }
+
+    @Override
+    public void addWizardListener(WizardListener listener) {
+        if (listener != null) {
+            allWizardListeners.add(listener);
+        }
+    }
+
+    @Override
+    public void removeWizardListener(WizardListener listener) {
+
+        if (!beeing_in_update_state) {
+            allWizardListeners.remove(listener);
+        } else {
+            toRemoveListeners.add(listener);
+        }
+    }
+
+    @Override
+    public void updateWizardListeners(WizardStatus currentWizardStatus) {
+
+        beeing_in_update_state = true;
+
+        System.out.println("UPDATE WIZARD LISTENER");
+
+        for (WizardListener currentListener : allWizardListeners) {
+            if (currentListener.onStateChange(currentWizardStatus) == false) {
+                if (!toRemoveListeners.contains(currentListener)) {
+                    toRemoveListeners.add(currentListener);
                 }
             }
+        }
 
-            beeing_in_update_state = false;
+        beeing_in_update_state = false;
 
-            /*
-             * Das müss ma so machen, weil wenn beim updateEvent einer sich nun
-             * deregistrieren will, und removeWizardListener aufruft kommts zu einer
-             * ConcurrentModificationException un nix passiert.
-             */
-            for( WizardListener listener : toRemoveListeners )
-            {
-                removeWizardListener(listener);
-            }
+        /*
+         * Das müss ma so machen, weil wenn beim updateEvent einer sich nun
+         * deregistrieren will, und removeWizardListener aufruft kommts zu einer
+         * ConcurrentModificationException un nix passiert.
+         */
+        for (WizardListener listener : toRemoveListeners) {
+            removeWizardListener(listener);
+        }
 
-	}
-
-	
+    }
 }
