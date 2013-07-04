bf642e5675dbccd2723eab4966aef0517fbfe043
martin <martin@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2013-07-04 21:27:23 +0000

    Die Mäglichkeit für einen eigenen Exceptionhandler eingebaut.
    git-svn-id: https://svn.hoffer.cx/repos/redeye/FrameWork/Main/trunk@1979 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/FrameWork/base/AutoMBox.java
+++ at/redeye/FrameWork/base/AutoMBox.java
@@ -8,6 +8,7 @@ package at.redeye.FrameWork.base;
 import org.apache.log4j.Logger;
 
 import at.redeye.FrameWork.utilities.StringUtils;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -17,11 +18,23 @@ import javax.swing.JOptionPane;
  */
 public abstract class AutoMBox
 {
+    public interface ShowAdvancedException
+    {
+        /**         
+         * @param ex
+         * @return false, if the default exceptiondialog should be shown
+         */        
+        boolean wantShowAdvancedException( Exception ex );        
+        void showAdvancedException( Exception ex );
+    }
+    
     protected Logger logger;    
     protected Exception thrown_ex = null;
     protected boolean failed = true;
     protected boolean do_mbox = true;
-    public boolean logical_failure = false;
+    public boolean logical_failure = false;        
+    
+    protected static ArrayList<ShowAdvancedException> show_exception_handlers = null;
     
     public AutoMBox( String className, boolean do_mbox )
     {
@@ -35,6 +48,14 @@ public abstract class AutoMBox
         logger = Logger.getLogger(className);
         
         invoke();
+    }
+    
+    public static void addShowAdvancedExceptionHandle( ShowAdvancedException handler )
+    {
+        if( show_exception_handlers == null )
+            show_exception_handlers = new ArrayList();
+        
+        show_exception_handlers.add(handler);
     }
 
     private void invoke()
@@ -50,14 +71,30 @@ public abstract class AutoMBox
         if (thrown_ex != null) {
             if (do_mbox) {
 
-                Root root = Root.getLastRoot();
+                boolean show_default_dialog = true;
+                
+                if( show_exception_handlers != null )
+                {
+                    for( ShowAdvancedException handler : show_exception_handlers )
+                    {
+                        if( handler.wantShowAdvancedException(thrown_ex) ) {
+                            show_default_dialog = false;
+                            handler.showAdvancedException(thrown_ex);
+                        }
+                    }
+                }
+               
+               
+                if (show_default_dialog) {
+                    Root root = Root.getLastRoot();
 
-                JOptionPane.showMessageDialog(null,
-                        StringUtils.autoLineBreak(
-                        root.MlM("Es ist ein Fehler aufgetreten:") + " " +
-                        thrown_ex.getLocalizedMessage()),
-                        root.MlM("Error"),
-                        JOptionPane.OK_OPTION);
+                    JOptionPane.showMessageDialog(null,
+                            StringUtils.autoLineBreak(
+                            root.MlM("Es ist ein Fehler aufgetreten:") + " "
+                            + thrown_ex.getLocalizedMessage()),
+                            root.MlM("Error"),
+                            JOptionPane.OK_OPTION);
+                }
             }
         }
     }
